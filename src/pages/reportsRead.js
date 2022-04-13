d8fb943863f575b9502ad8178df8e8f9d735dfc0
root <root@DESKTOP-UJLHJDH.localdomain>
2022-04-14 00:43:05 +1000

    Implementing report read

--- src/pages/reportsRead.js
+++ src/pages/reportsRead.js
@@ -1,101 +1,30 @@
-import React from 'react';
-import './wrapper.css';
+import React, { useEffect } from 'react';
 import axios from 'axios';
-import './wrapper.css';
-import PropTypes from 'prop-types';
-
 import Credentials from '../Credentials';
-import {
-  Box,
-  Button,
-  Container,
-  TextField,
-  Typography,
-} from '@material-ui/core';
 
-function reportsRead( {...props} ) {
-    const [query_str, setQueryStr] = React.useState("");
+function ListReport() {
+    const [reports, setReport] = React.useState();
 
-    const token = React.useContext(Credentials);
-  
-    function handleSubmit(event) {
-    event.preventDefault();
-
-    setSelectedFile(event.target[0].files[0]);
-    const file = selectedFile;
-    const report_type = event.target[2].value;
-    if (!file || !report_type) return;
-
-    const formData = new FormData();
-
-    formData.append('token', token.replace(/['"]+/g, ''));
-    formData.append('file', file);
-    formData.append('report_type', report_type);
-    
-    axios({
-      method: "post",
-      url: `/invoice/upload/API`,
-      data: formData,
-      headers: { "Content-Type": "multipart/form-data" },
-    })
-      .then((response) => {
-        console.log(response.data);
-        const report_id = response.data['report_id'];
-        const queryString = '?token=';
-        queryString.concat(token);
-        queryString.concat('&report_id');
-        queryString.concat(report_id);
-        props.history.push({
-          pathname: '/reports/read',
-          search: queryString,
-          state: { report_type }
-        });
-  
-        // const data = response.data;
-      })
-      .catch((error) => { console.error('Error, error') });
+    const tmp = React.useContext(Credentials);
+    const token = tmp.replace(/['"]+/g, '');
+    const report_id = window.location.search;
+    const params = new URLSearchParams();
+    params.append("token", token);
+    params.append("report_id",report_id);
+   
+    useEffect(() => {
+      fetchreports();
+    }, [])
+    useEffect(() => {
+      console.log(reports)
+    }, [reports])
+    const fetchreports=async()=>{
+        const response=await axios(`/reports/read`, { params});
+        setReport(response.data)    
     }
-
-  return(
-    <Container component="main" maxWidth="sm">
-      <Box boxShadow={1}>
-        <Typography component="h1" variant="h2">
-          Upload Invoice
-        </Typography>
-        {
-          <form onSubmit={handleSubmit}>
-            <TextField
-              variant="outlined"
-              margin="normal"
-              required
-              fullWidth
-              id="file"
-              name="file"
-              type="file"
-              autoFocus 
-            />
-            <TextField
-              variant="outlined"
-              margin="normal"
-              required
-              fullWidth
-              id="report_type"
-              name="Report Type"
-              label="Report Type"
-              type="text"
-              autoFocus 
-            />
-            <Button type="submit" fullWidth variant="contained" color="primary">
-            Submit
-            </Button>
-            </form>
-        }
-      </Box>
-    </Container>
+    return (
+      <div dangerouslySetInnerHTML={{ __html: {reports} }} />
   );
 }
-
-export default reportsRead;
-// UploadInvoice.propTypes = {
-//   setSelectedFile: PropTypes.func.isRequired
-// }
+   
+export default ListReport;
